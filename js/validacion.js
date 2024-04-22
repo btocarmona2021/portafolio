e4fe1b6fba064a2aabdaa79c679e46cc112c527b
Martin <bustincho@gmail.com>
2024-04-22 18:55:53 -0300

    Tercer Commit

--- js/validacion.js
+++ js/validacion.js
@@ -1,1 +1,11 @@
-//Haz tú validación en javascript acá
+const nombre = document.getElementById('nombre');
+const email = document.getElementById('email');
+const asunto = document.getElementById('asunto');
+const texto = document.getElementById('mensagem');
+const botonSubmit = document.querySelector('.formcontacto__boton');
+
+botonSubmit.addEventListener('click',()=>{
+    if (nombre.value =='' || email.value == '' || asunto.value == '' || texto.value == ''){
+        alert("Hay campos sin completar , Intentalo nuevamente !")
+    }
+})

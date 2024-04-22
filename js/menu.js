1bd50356b0ba82e5bdb83d9e6cfd3640c4fc6049
Martin <bustincho@gmail.com>
2024-04-22 07:06:15 -0300

    Primer Commit

--- js/menu.js
+++ js/menu.js
@@ -2,7 +2,6 @@ document.addEventListener('mousemove', function (ev) {
     let currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
     const perfil = document.querySelector('.contenedor__aside');
     const central = document.querySelector('.central');
-    const logo = document.querySelector('.logo');
     if (ev.clientX < 240 && currentScrollTop < 270) {
         perfil.classList.add('active');
         central.classList.add('left');

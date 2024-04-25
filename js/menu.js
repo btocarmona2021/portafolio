195eae470a264b6ed83910b3082948c4da3cdff2
Martin <bustincho@gmail.com>
2024-04-24 23:40:48 -0300

    Quinto Commit Responsive

--- js/menu.js
+++ js/menu.js
@@ -2,7 +2,7 @@ document.addEventListener('mousemove', function (ev) {
     let currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
     const perfil = document.querySelector('.contenedor__aside');
     const central = document.querySelector('.central');
-    if (ev.clientX < 240 && currentScrollTop < 270) {
+    if (ev.clientX < 240 && currentScrollTop < 270 && window.innerWidth > 768) {
         perfil.classList.add('active');
         central.classList.add('left');
     } else {
--- js/validacion.js
+++ js/validacion.js
@@ -3,10 +3,13 @@ const email = document.getElementById('email');
 const asunto = document.getElementById('asunto');
 const texto = document.getElementById('mensagem');
 const botonSubmit = document.querySelector('.formcontacto__boton');
+const contenedorFormulario = document.querySelector('.formcontacto__form');
 
-botonSubmit.addEventListener('click',()=>{
-    if (nombre.value =='' || email.value == '' || asunto.value == '' || texto.value == ''){
-        alert("Hay campos sin completar , Intentalo nuevamente !")
-
+botonSubmit.addEventListener('click', (ev) => {
+    if (nombre.value == '' || email.value == '' || asunto.value == '' || texto.value == '') {
+        const error = document.createElement('h3');
+        error.innerText = 'Hay campos sin completar , Intentalo nuevamente !'
+        contenedorFormulario.appendChild(error);
+        ev.preventDefault();
     }
 })
